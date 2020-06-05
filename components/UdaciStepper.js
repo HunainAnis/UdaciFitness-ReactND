11846fb89932cea01910c229f02070eb9a14f54b
Hunain Anis <hunainmuhammad@rocketmail.com>
2020-06-05 22:10:28 +0500

    increment, decrement buttons are styled and both of text are also styled!

--- components/UdaciStepper.js
+++ components/UdaciStepper.js
@@ -1,23 +1,57 @@
 import React from 'react'
-import { View, Text } from 'react-native'
+import { View, Text, StyleSheet, Platform } from 'react-native'
 import { TouchableOpacity } from 'react-native-gesture-handler'
 import { FontAwesome } from '@expo/vector-icons'
+import { white, purple, gray } from '../utils/colors'
 
 export default function UdaciSteppers({ value, unit, step, max, onDecrement, onIncrement }) {
         return(
-            <View>
-                <View>
-                    <TouchableOpacity onPress={onDecrement} >
-                        <FontAwesome name='minus' color='black' size={30} />
+            <View style={[styles.row, {justifyContent: 'space-between'}]}>
+                <View style={{flexDirection:'row'}}>
+                    <TouchableOpacity 
+                        onPress={onDecrement}
+                        style={[Platform.OS === 'ios' ? styles.iosBtn: styles.androidBtn, {borderTopRightRadius: 0, borderBottomRightRadius: 0}]} >
+                        <FontAwesome name='minus' color={Platform.OS === 'ios'? purple : white} size={30} />
                     </TouchableOpacity>
-                    <TouchableOpacity onPress={onIncrement} >
-                        <FontAwesome name='plus' color='black' size={30} />
+                    <TouchableOpacity 
+                        onPress={onIncrement}
+                        style={[Platform.OS === 'ios' ? styles.iosBtn: styles.androidBtn, {borderTopLeftRadius: 0, borderBottomLeftRadius: 0}]} >
+                        <FontAwesome name='plus' color={Platform.OS === 'ios'? purple : white} size={30} />
                     </TouchableOpacity>
                 </View>
-                <View>
-                    <Text>{value}</Text>
-                    <Text>{unit}</Text>
+                <View style={styles.metricCounter}>
+                    <Text style={{fontSize: 24, textAlign: 'center'}}>{value}</Text>
+                    <Text style={{fontSize: 18, color: gray}}>{unit}</Text>
                 </View>
             </View>
         )
     }
+
+    const styles = StyleSheet.create({
+        row: {
+            flexDirection: 'row',
+            flex: 1,
+            alignItems: 'center'
+        },
+        iosBtn: {
+            backgroundColor: white,
+            borderColor: purple,
+            borderRadius: 3,
+            borderWidth: 1,
+            padding: 5,
+            paddingLeft: 25,
+            paddingRight: 25
+        },
+        androidBtn: {
+            margin: 5,
+            backgroundColor: purple,
+            padding: 10,
+            borderRadius: 2
+        },
+        metricCounter: {
+            width: 85,
+            justifyContent: 'center',
+            alignItems: 'center',
+
+        }
+    })
