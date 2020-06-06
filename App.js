aa69f4771b3a4fcf7de30d727afcde7d370b8568
Hunain Anis <hunainmuhammad@rocketmail.com>
2020-06-06 15:08:45 +0500

    Navigation tabs are added and App works fine!

--- App.js
+++ App.js
@@ -1,10 +1,15 @@
+import 'react-native-gesture-handler';
 import * as React from 'react';
-import { Platform, StyleSheet, View } from 'react-native';
+import { Platform, StyleSheet, View, StatusBar } from 'react-native';
 import AddEntry from './components/AddEntry';
 import { Provider } from 'react-redux';
 import { createStore } from 'redux';
 import reducer from './reducers'
 import History from './components/History';
+import { NavigationContainer } from '@react-navigation/native';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { Constants } from 'react-native-unimodules';
+import { purple } from './utils/colors';
 // import { YellowBox } from 'react-native';
 // YellowBox.ignoreWarnings(['Remote debugger']);
 
@@ -14,15 +19,42 @@ const instructions = Platform.select({
 });
 
 export default class App extends React.Component {
+  
   render() {
-  return (
-    <Provider store={createStore(reducer)}>
-      <View style={{flex:1}}>
+    return (
+      <Provider store={createStore(reducer)}>
+      {/* <View style={{flex:1}}>
         <View style={{height:20}} />
+        <NavigationContainer>
+          <MyTabs />
+        </NavigationContainer> */}
           {/* <AddEntry /> */}
-          <History />
-      </View>
+          {/* <History /> */}
+      {/* </View> */}
+      <UdaciStatusBar backgroundColor={purple} barStyle='light-content' />
+      <NavigationContainer>
+      <MyTabs />
+    </NavigationContainer>
     </Provider>
   );
 }
 }
+
+function UdaciStatusBar ({ backgroundColor, ...props }) {
+  return(
+    <View style={{backgroundColor, height:Constants.statusBarHeight}}>
+      <StatusBar translucent backgroundColor={backgroundColor} {...props} />
+    </View>
+  )
+}
+
+function MyTabs() {
+  const Tab = createBottomTabNavigator();
+  return (
+    <Tab.Navigator>
+      <Tab.Screen name="History" component={History} />
+      <Tab.Screen name="Add Data" component={AddEntry} />
+    </Tab.Navigator>
+  );
+}
+
